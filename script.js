d294c9c35375aecb56e3c4c968284fd36730ec49
Redjul2110 <julian.fischer1@t-online.de>
2026-01-31 20:47:33 +0100

    script.js löschen

--- script.js
+++
@@ -1,6 +0,0 @@
-// Sofortige Weiterleitung nach ca. 2 Sekunden
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        window.location.replace("https://redjgames.base44.app");
-    }, 2000); // 2 Sekunden
-});
