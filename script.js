9f53c8d2ebd7bb0416f73fded99ff4e75bf79a12
Redjul2110 <julian.fischer1@t-online.de>
2026-01-31 20:44:54 +0100

    script.js aktualisieren

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-// Nach 5 Minuten (300.000ms) automatisch weiterleiten
-setTimeout(function() {
-    window.location.replace("https://redjgames.base44.app");
-}, 300000); // 5 Minuten
-
-// Optional: direkt nach kurzer Zeit schon redirecten
-// setTimeout(function(){
-//     window.location.replace("https://redjgames.base44.app");
-// }, 2000); // 2 Sekunden, wenn Nutzer nicht warten soll
+// Sofortige Weiterleitung nach ca. 2 Sekunden
+window.addEventListener('load', () => {
+    setTimeout(() => {
+        window.location.replace("https://redjgames.base44.app");
+    }, 2000); // 2 Sekunden
+});
